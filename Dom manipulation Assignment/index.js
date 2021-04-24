4753773102e801832ac08950ce71649edfaab7c1
Anonymous655 <55239017+Anonymous655@users.noreply.github.com>
2021-04-24 15:05:06 +0530

    getByClassName

--- Dom manipulation Assignment/index.js
+++ Dom manipulation Assignment/index.js
@@ -9,3 +9,12 @@ const addItems = document.getElementById("title");
 header.style.border = "solid 3px black";
 addItems.style.fontWeight = "bold";
 addItems.style.color = "green";
+
+// task 2
+const items = document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor = 'green';
+
+for(i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
