feef3e9ce1ba1920b6dac5398804a1738fa2cc46
Anonymous655 <55239017+Anonymous655@users.noreply.github.com>
2021-04-25 13:45:20 +0530

    added task 4

--- Dom manipulation Assignment/index.js
+++ Dom manipulation Assignment/index.js
@@ -1,5 +1,5 @@
 // to show the whole DOM
-console.log(document);
+// console.log(document);
 
 // innerText returns the visible text contained in a node, while textContent returns the full text. For example, on the following HTML <span>Hello <span style="display: none;">World</span></span>, innerText will return 'Hello', while textContent will return 'Hello World'.
 
@@ -32,3 +32,45 @@ newItem[0].style.listStyle = 'none';
 
 newItemTag[4].style.paddingLeft = '15px';
 newItemTag[4].style.padding = '15px';
+
+
+
+// task 4
+
+const ulList = document.getElementById('items');
+ulList.parentElement.style.border = '1px solid black';
+ulList.lastElementChild.style.color = 'yellow';
+// ulList.lastChild.style.border = '1x solid black'; note: It returns the text node
+ulList.firstElementChild.style.color = 'red';
+//  firstchild returns the text node
+// nextSibling also returns the text node
+// previousSibling also returns text node
+ulList.previousElementSibling.style.color  = 'green'
+
+
+let textNode = document.createTextNode('Hello world');
+let newdiv = document.createElement('div');
+newdiv.appendChild(textNode);
+const container = document.querySelector('header .container');
+const headerTitle = document.querySelector('header h1');
+console.log(newdiv);
+container.insertBefore(newdiv, headerTitle);
+
+
+const textNode2 = document.createTextNode("Hello World")
+const newList = document.createElement('li');
+newList.appendChild(textNode2);
+newList.setAttribute('class', 'newlist');
+let item1 = document.querySelector('ul #first')
+ulList.insertBefore(newList, item1)
+console.log(newList)
+let newlist = document.getElementsByClassName('newlist');
+newlist[0].style.fontWeight = 'bold'
+newlist[0].style.paddingLeft = '15px'
+newlist[0].style.listStyle = 'none'
+newlist[0].style.padding = '15px'
+
+
+
+
+
